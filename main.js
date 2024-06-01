9d84f907d7949daedf2136fea634505e48fb44b8
dewinson2 <166655551+dewinson2@users.noreply.github.com>
2024-05-31 23:59:43 -0400

    Update main.js

--- main.js
+++ main.js
@@ -4,31 +4,40 @@ let AC = document.getElementById("AC");
 let igual = document.getElementById("igual");
 
 let primerClick = true;
+
+// Event listener para la entrada en pantalla
 pantalla.addEventListener("input", function() {
     primerClick = false;
-
-    
-
 });
+
 btns.forEach(function(btn) {
     btn.addEventListener("click", function() {
+        // Limpiar la pantalla si es el primer clic
         if (primerClick) {
             pantalla.textContent = "";
             primerClick = false;
         }
 
-        // Verificar si el botón es un operador
-        if (btn.textContent === "=") {
+        // Verificar si el botón es el botón "C" para borrar el último carácter
+        if (btn.textContent === "C") {
+            pantalla.textContent = pantalla.textContent.slice(0, -1);
+        }
+        // Verificar si el botón es el botón "=" para evaluar la expresión
+        else if (btn.textContent === "=") {
             try {
                 pantalla.textContent = eval(pantalla.textContent);
                 primerClick = true; // Restablecer la bandera después de "="
             } catch (error) {
                 pantalla.textContent = "Error";
             }
-        } else if (btn.textContent === "AC") {
+        }
+        // Verificar si el botón es el botón "AC" para limpiar la pantalla
+        else if (btn.textContent === "AC") {
             pantalla.textContent = "";
             primerClick = true; // Restablecer la bandera al borrar
-        } else {
+        }
+        // Para todos los demás botones, agregar su contenido de texto a la pantalla
+        else if (btn.textContent !== "C") {
             pantalla.textContent += btn.textContent;
         }
     });
